0be63e744da3116ef33f83d7ed738a0e6756d7d3
vaishnavipatil978 <115615039+vaishnavipatil978@users.noreply.github.com>
2023-04-03 11:59:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function threeSum(arr, target) {
 // write your code here
+
+	let distance = 999999;
+	let ans = 0;
+
+	for(let i = 0 ; i < arr.length-2 ; i++){
+		for(let j = i+1 ; j < arr.length-1 ; j++){
+			for(let k = j+1 ; k < arr.length ; k++){
+				let sum = arr[i]+arr[j]+arr[k];
+
+				if(Math.abs(sum-target)<distance){
+					distance = Math.abs(sum-target);
+					ans = sum;
+				}
+			}
+		}
+	}
+
+	return ans;
   
 }
 
